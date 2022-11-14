bf8d39f33ea7a47467cbe7d18734d045b21ce70d
unknown <Darus Salam Sakib>
2022-11-14 23:21:40 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const init = async () => {
+
+    const server = Hapi.server({
+        host: 'localhost',
+        port: 3000
+        
+    });
+
+    server.route({
+        method: `GET`,
+        path: `/`,
+        handler: (request, h) => {
+            return "<h1>Hello World!</h1><br><h2>Ki obosthaa borobhai</h2>";
+        }
+    });
+
+    await server.start();
+    console.log(`Server started on: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+
+    console.log(err);
+    process.exit(1);
+})
+
+init();
